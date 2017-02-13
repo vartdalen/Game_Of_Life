1cc583ecd0c989958d11a1f364631acbbaf73fd1
Vartdalen <s315615@stud.hioa.no>
2017-02-13 17:38:31 +0100

    test test

--- Game of Life/src/gui_controller.java
+++ Game of Life/src/gui_controller.java
@@ -26,7 +26,7 @@ public class gui_controller implements Initializable {
 	@Override
 	public void initialize(java.net.URL location,
             java.util.ResourceBundle resources) {
-		
+		System.out.println("test");
 	}
 	
 }

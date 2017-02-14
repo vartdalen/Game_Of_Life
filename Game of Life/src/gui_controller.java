0a3141cb908f6aeb44afb16ce352b937942d5d87
tguest89 <s305457@stud.hioa.no>
2017-02-14 16:48:18 +0100

    Lagt til (i gui_controller): Skrive til canvas Vise mønster i canvas + endre farge og størrelse på mønster (cellene). Skisset opp mulige metoder vi trenger å utfylle.
    Ryddet i gui_main.

--- Game of Life/src/gui_controller.java
+++ Game of Life/src/gui_controller.java
@@ -1,41 +1,118 @@
-import java.net.URL;
+//import java.net.URL;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.ResourceBundle;
+//import java.util.ResourceBundle;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.canvas.Canvas;
 import javafx.scene.canvas.GraphicsContext;
-import javafx.scene.control.Button;
-import javafx.scene.control.ColorPicker;
-import javafx.scene.control.MenuBar;
+//import javafx.scene.control.Button;
+//import javafx.scene.control.ColorPicker;
+//import javafx.scene.control.MenuBar;
 import javafx.scene.control.Slider;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.paint.Color;
-import javafx.scene.shape.ArcType;
 
 public class gui_controller implements Initializable {
 
+	private byte [][] board = { //Mønster
+			{1,0,0,1}, 
+			{0,1,1,0}, 
+			{0,1,1,0}, 
+			{1,0,0,1}
+		   };
+	
+	private double size = 70.0; //Cell-size
+	
+	private static class Point {
+		public double x,y;
+		public void draw(GraphicsContext gc, double size) {
+			gc.setFill(Color.GREEN);
+			gc.fillRect(x, y, size, size);
+		}
+	}
+	
+//	@FXML private Button btnStart;
+//	@FXML private Button btnStop;
+//	@FXML private Button btnClear;
+//	@FXML private Button btnImport;
+	@FXML private Slider slider_size;
+	@FXML private Slider slider_speed;
+	@FXML private Canvas gol_canvas;
+	private List<Point> plist;
+	
+	public void initialize(java.net.URL location,
+            java.util.ResourceBundle resources) {
+		slider_size.setValue(5.0);
+		slider_speed.setValue(5.0);
+		plist = new ArrayList<Point>();
+		draw();
+	}
+	
+	private void draw() { //tegner opp mønster
+		GraphicsContext gc = gol_canvas.getGraphicsContext2D();
+		Point p = new Point();
+		
+		for(int i = 0; i<board.length; i++) {
+			for(int j = 0; j<board[i].length; j++){
+				if(board[i][j] == 1) {
+				p.y = i*size;
+				p.x = j*size;
+				p.draw(gc, size);
+				}
+			}
+		}
+		gc.strokeText("Hello canvas", 150, 450);
+//		gc.clearRect(0, 0, gol_canvas.widthProperty().doubleValue(), gol_canvas.heightProperty().doubleValue());
+//		for( Point p : plist) {
+//			p.draw(gc, Color.RED,slider_size.getValue());
+//		}
+	}
+	
+	public void startBtnClicked(ActionEvent e) {
+		/*
+		 * Starte generering/forandring av celler
+		 * */
+	}
+	
+	public void stopBtnClicked(ActionEvent e) {
+		/*
+		 * Stoppe generering/forandring av celler
+		 * */
+	}
+	
+	public void clearBtnClicked(ActionEvent e) {
+		/*
+		 * fjerne eksisterende celler -> blanke ark.
+		 * */
+	}
+	
+	public void changeSpeed(ActionEvent e) {
+		/*
+		 * Endre hastighet på celle generering
+		 * */
+	}
+	
+	public void changeSize(ActionEvent e) {
+		/*
+		 * Endre størrelse på cellene
+		 * */
+	}
+	
+	public void importBtnClicked(ActionEvent e) {
+		/*
+		 * Kunne importere andre mønstre
+		 * */
+	}
+	
 	public void exitEvent(ActionEvent event) {
 		System.exit(0);
 	}
 	
 	public void heyEvent(ActionEvent event) {
 		System.out.println("helloooooo");
-	}
-	
-	/*@FXML private Button btnStart;
-	@FXML private Button btnStop;
-	@FXML private Button btnClear;
-	@FXML private Slider slider_size;
-	@FXML private Slider slider_speed;
-	@FXML private Canvas graphics;*/
-	
-	public void initialize(java.net.URL location,
-            java.util.ResourceBundle resources) {
-		
 	}
 	
 	
--- Game of Life/src/gui_main.java
+++ Game of Life/src/gui_main.java
@@ -12,26 +12,15 @@ public class gui_main extends Application {
 	@Override
 	public void start(Stage primaryStage) throws Exception {
 		
-//		Group root = new Group();
 		Parent root = FXMLLoader.load(getClass().getResource("gol_gui3.fxml"));
 		
-		Scene scene = new Scene(root, 800, 600);
-		
-//		Canvas canvas = new Canvas(700, 600);
-//		GraphicsContext gc = canvas.getGraphicsContext2D();
-//		drawShapes(gc);
-//		root.getChildren().add(canvas);
-//		Heyhey
+		Scene scene = new Scene(root);
 		
 		primaryStage.setTitle("Game of Life");
 		primaryStage.setScene(scene);
 		primaryStage.show();
 		
 	}
-	
-//	private void drawShapes(GraphicsContext gc) {
-//		gc.setFill(Color.GREEN);
-//	}
 	
 	public static void main(String[] args) {
 		launch(args);

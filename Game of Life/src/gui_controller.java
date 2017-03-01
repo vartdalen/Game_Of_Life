5f62526b0c9949edab4e588c3f1388e8705d6e00
Vartdalen <s315615@stud.hioa.no>
2017-03-01 16:14:32 +0100

    eksperimentering på diverse funksjoner med timeline

--- Game of Life/src/gui_controller.java
+++ Game of Life/src/gui_controller.java
@@ -32,7 +32,7 @@ public class gui_controller implements Initializable {
 		   };
 	
 	private byte [][] board = cloneByteArray(initialBoard);
-	private double size = 70.0; //Cell-size
+	//private float size = 5000; //Cell-size
 	/*
 	 * Vi bruker verdiene i Board til Ã¥ avgjÃ¸re hvilken farge som blir brukt. Verdi 0 i Board = White fordi White 
 	 * er plassert pÃ¥ index [0] i "colors" tabellen under; Verdi 1 i board = black fordi den er plassert pÃ¥ index[1].
@@ -57,16 +57,55 @@ public class gui_controller implements Initializable {
 	 * UtfÃ¸rer spill-regelen: Hvis celle er "dÃ¸d"- vekk den til live (ved Ã¥ gi den "1" verdi), og drep celle dersom
 	 * den er i live (ved Ã¥ sette verdien dens til 0).
 	 */
+	
+	//prøvde å lage gol-regler ift naboer, fikk noen exception errors. regner med at det har noe å gjøre med at celler som befinner seg
+	// i endene av gridden ikke finner naboer. eller at jeg må forandre loopen fra å søke gjennom samtlige celler i gridden og heller kun søke de
+	// 8 nærmeste.
+	
 	private void flipValues() {
 		for (int i = 0; i < board.length; i++) {
 			for (int j = 0; j < board[i].length; j++) {
 				if (board[i][j] == 1) {
+					
 					board[i][j] = 0;
+					
 				} else if (board[i][j] == 0) {
+					
 					board[i][j] = 1;
+					
 				}
 			}
 		}
+		
+//		for (int i = 0; i < board.length; i++) {
+//			for (int j = 0; j < board[i].length; j++) {
+//				if (board[i-1][j-1] + board[i][j-1] + 
+//					board[i+1][j-1] + board[i+1][j] +
+//					board[i+1][j+1] + board[i][j+1] + 
+//					board[i-1][j+1] + board[i-1][j] == 3) {
+//					
+//					board[i][j] = 1; //cellen blir født
+//					
+//				} else if 	(board[i-1][j-1] + board[i][j-1] + 
+//							board[i+1][j-1] + board[i+1][j] +
+//							board[i+1][j+1] + board[i][j+1] + 
+//							board[i-1][j+1] + board[i-1][j] > 3) {
+//					
+//					board[i][j] = 0; //cellen dør av overpopulasjon
+//					
+//				} else if	(board[i-1][j-1] + board[i][j-1] + 
+//							board[i+1][j-1] + board[i+1][j] +
+//							board[i+1][j+1] + board[i][j+1] + 
+//							board[i-1][j+1] + board[i-1][j] < 3) {
+//					
+//					board[i][j] = 0; // cellen dør av ensomhet
+//					
+//				}
+//			}
+//		}
+		
+		
+		
 	}
 	
 //	private void blackify() {
@@ -89,14 +128,12 @@ public class gui_controller implements Initializable {
 	@FXML private Canvas gol_canvas;
 //	private List<Point> plist;
 	
-	private float getAnimationSpeed() {
-		return (float) slider_speed.getValue();
-	}
+	
 	
 	public void initialize(java.net.URL location,
             java.util.ResourceBundle resources) {
-		slider_size.setValue(5.0);
-		slider_speed.setValue((slider_speed.getMin() + slider_speed.getMax()) / 2);
+		slider_size.setValue(slider_size.getValue());
+		slider_speed.setValue((slider_speed.getValue()));
 		slider_speed.valueProperty().addListener(new ChangeListener<Number>(){
 
 			@Override
@@ -110,6 +147,8 @@ public class gui_controller implements Initializable {
 			}});
 //		plist = new ArrayList<Point>();
 		drawBoard();
+		clearCanvas(); //prøver å bli kvitt søppel som blir igjen i bakgrunnen.
+		//dette gjør bare at funksjonen starter blankt.
 	}
 	
 //	private void draw() { //tegner opp mÃ¸nster
@@ -137,11 +176,11 @@ public class gui_controller implements Initializable {
 		
 		for(int i = 0; i < board.length; i++) {
 			for(int j = 0; j < board[i].length; j++) {
-				double x = j*size;
-				double y = i*size;
+				double x = j*getCellSize();
+				double y = i*getCellSize();
 				byte boardValue = board[i][j];
 				gc.setFill(colors[boardValue]); 
-				gc.fillRect(x, y, size, size);
+				gc.fillRect(x, y, getCellSize(), getCellSize());
 			}
 		}
 	}
@@ -203,7 +242,7 @@ public class gui_controller implements Initializable {
 	
 	void clearCanvas() {
 		GraphicsContext gc = gol_canvas.getGraphicsContext2D();
-		gc.clearRect(0, 0, board.length * size, board[0].length * size);
+		gc.clearRect(0, 0, board.length * slider_size.getMax(), board[0].length * slider_size.getMax());
 	}
 	
 	public void clearBtnClicked(ActionEvent e) {
@@ -212,6 +251,17 @@ public class gui_controller implements Initializable {
 		 * */
 		clearCanvas();
 		board = cloneByteArray(initialBoard);
+	}
+	
+	public float getCellSize() {
+		
+		//forandrer lengde og høyde på cellene
+		return (float)slider_size.getValue();
+		
+	}
+	
+	private float getAnimationSpeed() {
+		return (float) slider_speed.getValue();
 	}
 	
 	public void importBtnClicked(ActionEvent e) {
